2d9d3eda46531bd6ba85f7d93c545632d3623e36
Francesco Caruso <francesco.caruso@spezia.wsse.localdomain>
2015-05-08 08:28:57 +0200

    glsl shaders removed from project directories. Now they are only in the glsl directory

--- BezierCurve/fragment.glsl
+++
@@ -1,7 +0,0 @@
-#version 440
-
-void
-main()
-{
-	gl_FragColor = vec4(0., 0., 1., 1.);
-}
--- PlaneCurve/f_plane_curve.glsl
+++
@@ -1,9 +0,0 @@
-#version 440
-
-out vec4 fColor;
-
-void
-main()
-{
-	 fColor =  vec4( 0.0, 0.8, 0.0, 1.0 );
-}
